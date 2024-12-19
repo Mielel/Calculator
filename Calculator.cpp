b4c340c903cde3341073f9bac70d616026703cd9
Mielel <dhimasaryahgit@gmail.com>
2024-12-19 17:44:48 +0700

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -14,7 +14,36 @@ struct Operator{
     int posisiOperator;
 };
 
-bool cekValiditas(string Operasi){
+bool cekValiditas(const string Operasi);
+void masukkanString(string Operasi);
+void pecahkanOperasi(const string Operasi, vector<Angka> *numbers, vector<Operator> *sign, vector<int> *posisiKaliBagi);
+
+void lakukanPerkalianDanPembagian(vector<Angka> *numbers, vector<Operator> *sign, vector<int> *posisiKaliBagi){
+    
+}
+
+void lakukanPenjumlahanDanPengurangan(vector<Angka> *numbers, vector<Operator> *sign, int *hasil){
+
+}
+
+int main(){
+    string Operasi; // Variabel yang digunakan untuk menerima input
+    vector<Angka> numbers; // Variabel untuk menyimpan seluruh angka dan posisinya dalam operasi
+    vector<Operator> sign; // Variabel untuk menyimpan seluruh tanda dan posisinya dalam operasi 
+    vector<int> posisiKaliBagi; // Variabel untuk menyimpan posisi tanda kali dan bagi ada di mana
+    
+    masukkanString(Operasi);
+    pecahkanOperasi(Operasi, &numbers, &sign, &posisiKaliBagi);
+
+    lakukanPerkalianDanPembagian(&numbers, &sign, &posisiKaliBagi);
+
+    int hasil = 0;
+    lakukanPenjumlahanDanPengurangan(&numbers, &sign, &hasil);
+
+    return 0;
+}
+
+bool cekValiditas(const string Operasi){
     /* Function untuk memastikan :
     1. bahwa tidak ada inputan lain selain angka dan tanda operasi matematika (+, -, /, *)
     2. tidak berakhir dengan tanda operasi matematika. 
@@ -25,42 +54,18 @@ bool cekValiditas(string Operasi){
 
 }
 
-void masukkanString(string Operasi){ 
+void masukkanString(string *Operasi){ 
     // Prosedur untuk menerima input dari user
     cout << "Masukkan sebuah operasi matematika tanpa spasi: ";
-    cin >> Operasi; 
-    while(!cekValiditas(Operasi)){
+    cin >> *Operasi; 
+    while(!cekValiditas(*Operasi)){
     // Masukkan kembali input sampai sesuai dengan cekValiditas
         cout << endl;
         cout << "Ada kesalahan dalam input Anda.\nMasukkan kembali sebuah operasi matematika: ";
-        cin >> Operasi;
+        cin >> *Operasi;
     }
 }
 
-void pecahkanOperasi(string Operasi, vector<Angka> numbers, vector<Operator> sign, vector<int> posisiKaliBagi){
+void pecahkanOperasi(const string Operasi, vector<Angka> *numbers, vector<Operator> *sign, vector<int> *posisiKaliBagi){
     // Prosedur untuk mengubah operasi matematika ke angka-angka dan tanda-tandanya
-
-}
-
-void lakukanPerkalianDanPembagian(vector<Angka> numbers, vector<Operator> sign, vector<int> posisiKaliBagi);
-void lakukanPenjumlahanDanPengurangan(vector<Angka> numbers, vector<Operator> sign, int hasil);
-
-
-
-
-int main(){
-    string Operasi; // Variabel yang digunakan untuk menerima input
-    vector<Angka> numbers; // Variabel untuk menyimpan seluruh angka dan posisinya dalam operasi
-    vector<Operator> sign; // Variabel untuk menyimpan seluruh tanda dan posisinya dalam operasi 
-    vector<int> posisiKaliBagi; // Variabel untuk menyimpan posisi tanda kali dan bagi ada di mana
-    
-    masukkanString(Operasi);
-    pecahkanOperasi(Operasi, numbers, sign, posisiKaliBagi);
-
-    lakukanPerkalianDanPembagian(numbers, sign, posisiKaliBagi);
-
-    int hasil = 0;
-    lakukanPenjumlahanDanPengurangan(numbers, sign, hasil);
-
-    return 0;
 }
